75b9d2f83bd694a4410a6427474dfdec2b47982c
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-06-07 16:12:40 +0700

    Cart

--- jsnangcao/src/pages/StudentDetail.js
+++ jsnangcao/src/pages/StudentDetail.js
@@ -10,8 +10,28 @@ const StudentDetail = {
                 <div>ID: ${data.id}</div>
                 <div>Name: ${data.name}</div>
                 <div>MSV: ${data.msv}</div>
+                <div>
+                    <input type='number' id='cartValue' value='1' min='1' />
+                    <button
+                        class='btn btn-info'
+                        data-id="${data.id}"
+                        data-name="${data.name}"
+                        id='btn-add-cart'
+                    >Thêm vào giỏ hàng</button>
+                </div>
             </div>`
         );
+    },
+    afterRender: () => {
+        const btnAddCart = document.querySelector('#btn-add-cart');
+        btnAddCart.addEventListener('click', () => {
+            const item = {
+                id: btnAddCart.dataset.id,
+                name: btnAddCart.dataset.name,
+                value: +document.querySelector('#cartValue').value || 1
+            };
+            console.log(item);
+        });
     }
 };
 
